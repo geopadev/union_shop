27c39921922b56ca080ebcb0907bfe77187b1854
George Papageorgiou <g.papageorgiou005@gmail.com>
2025-11-27 15:03:21 +0000

    eat: wire up CollectionViewModel and CollectionRepository with DI

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:union_shop/repositories/in_memory_collection_repository.dart';
 import 'package:union_shop/repositories/in_memory_product_repository.dart';
+import 'package:union_shop/repositories/collection_repository.dart';
 import 'package:union_shop/repositories/product_repository.dart';
+import 'package:union_shop/view_models/collection_view_model.dart';
 import 'package:union_shop/view_models/home_view_model.dart';
 import 'package:union_shop/view_models/product_view_model.dart';
 import 'package:union_shop/views/about_view.dart';
+import 'package:union_shop/views/collections_view.dart';
 import 'package:union_shop/views/home_view.dart';
 import 'package:union_shop/views/product_view.dart';
 
@@ -16,22 +20,29 @@ void main() {
 /// This allows tests to inject mock repositories with zero latency
 Widget createApp({
   ProductRepository? productRepo,
+  CollectionRepository? collectionRepo,
   GlobalKey<NavigatorState>? navigatorKey,
 }) {
-  // Use provided repository or create default with 500ms latency
-  final repository = productRepo ?? InMemoryProductRepository();
+  // Use provided repositories or create defaults with 500ms latency
+  final productRepository = productRepo ?? InMemoryProductRepository();
+  final collectionRepository = collectionRepo ?? InMemoryCollectionRepository();
 
   return MultiProvider(
     providers: [
-      // Repository provider
-      Provider<ProductRepository>.value(value: repository),
+      // Repository providers
+      Provider<ProductRepository>.value(value: productRepository),
+      Provider<CollectionRepository>.value(value: collectionRepository),
 
       // ViewModel providers
       ChangeNotifierProvider<HomeViewModel>(
-        create: (_) => HomeViewModel(repository),
+        create: (_) => HomeViewModel(productRepository),
       ),
       ChangeNotifierProvider<ProductViewModel>(
-        create: (_) => ProductViewModel(repository),
+        create: (_) => ProductViewModel(productRepository),
+      ),
+      ChangeNotifierProvider<CollectionViewModel>(
+        create: (_) =>
+            CollectionViewModel(collectionRepository, productRepository),
       ),
     ],
     child: UnionShopApp(navigatorKey: navigatorKey),
@@ -57,6 +68,17 @@ class UnionShopApp extends StatelessWidget {
       routes: {
         '/product': (context) => const ProductPage(),
         '/about': (context) => const AboutPage(),
+        '/collections': (context) => const CollectionsPage(),
+      },
+      onGenerateRoute: (settings) {
+        // Handle dynamic routes with parameters
+        if (settings.name != null && settings.name!.startsWith('/shop/')) {
+          final collectionId = settings.name!.replaceFirst('/shop/', '');
+          return MaterialPageRoute(
+            builder: (context) => CollectionsPage(collectionId: collectionId),
+          );
+        }
+        return null;
       },
     );
   }
