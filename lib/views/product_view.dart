ca3724b4d939d7e96632a1926c96719c1bd5ee07
George Papageorgiou <g.papageorgiou005@gmail.com>
2025-11-27 12:54:52 +0000

    feat: replace placeholder footer with SharedFooter widget for consistent layout

--- lib/views/product_view.dart
+++ lib/views/product_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:union_shop/widgets/shared/shared_header.dart';
+import 'package:union_shop/widgets/shared/shared_footer.dart';
 
 class ProductPage extends StatelessWidget {
   const ProductPage({super.key});
@@ -122,19 +123,7 @@ class ProductPage extends StatelessWidget {
             ),
 
             // Footer
-            Container(
-              width: double.infinity,
-              color: Colors.grey[50],
-              padding: const EdgeInsets.all(24),
-              child: const Text(
-                'Placeholder Footer',
-                style: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ),
+            const SharedFooter(),
           ],
         ),
       ),
