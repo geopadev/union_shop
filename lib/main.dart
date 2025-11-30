5860f95fed962c26d3aa613e8f97f938c1e1c5ff
George Papageorgiou <g.papageorgiou005@gmail.com>
2025-11-30 01:12:40 +0000

    feat: wire CartViewModel into Provider for dependency injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:union_shop/repositories/cart_repository.dart';
+import 'package:union_shop/repositories/in_memory_cart_repository.dart';
 import 'package:union_shop/repositories/in_memory_collection_repository.dart';
 import 'package:union_shop/repositories/in_memory_product_repository.dart';
 import 'package:union_shop/repositories/collection_repository.dart';
 import 'package:union_shop/repositories/product_repository.dart';
 import 'package:union_shop/router/app_router.dart';
+import 'package:union_shop/view_models/cart_view_model.dart';
 import 'package:union_shop/view_models/collection_view_model.dart';
 import 'package:union_shop/view_models/home_view_model.dart';
 import 'package:union_shop/view_models/product_view_model.dart';
@@ -18,17 +21,20 @@ void main() {
 Widget createApp({
   ProductRepository? productRepo,
   CollectionRepository? collectionRepo,
+  CartRepository? cartRepo,
   GlobalKey<NavigatorState>? navigatorKey,
 }) {
   // Use provided repositories or create defaults with 500ms latency
   final productRepository = productRepo ?? InMemoryProductRepository();
   final collectionRepository = collectionRepo ?? InMemoryCollectionRepository();
+  final cartRepository = cartRepo ?? InMemoryCartRepository();
 
   return MultiProvider(
     providers: [
       // Repository providers
       Provider<ProductRepository>.value(value: productRepository),
       Provider<CollectionRepository>.value(value: collectionRepository),
+      Provider<CartRepository>.value(value: cartRepository),
 
       // ViewModel providers
       ChangeNotifierProvider<HomeViewModel>(
@@ -40,6 +46,9 @@ Widget createApp({
       ChangeNotifierProvider<CollectionViewModel>(
         create: (_) =>
             CollectionViewModel(collectionRepository, productRepository),
+      ),
+      ChangeNotifierProvider<CartViewModel>(
+        create: (_) => CartViewModel(cartRepository),
       ),
     ],
     child: UnionShopApp(navigatorKey: navigatorKey),
