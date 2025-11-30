0c5c9dafcf95c11841a18a2776628bd1232fc5a6
George Papageorgiou <g.papageorgiou005@gmail.com>
2025-11-30 17:29:15 +0000

    refactor: remove Firebase initialization code for cleaner main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
-import 'package:union_shop/firebase_options.dart';
+
 import 'package:union_shop/repositories/cart_repository.dart';
 import 'package:union_shop/repositories/in_memory_cart_repository.dart';
 import 'package:union_shop/repositories/in_memory_collection_repository.dart';
@@ -18,15 +18,12 @@ import 'package:union_shop/services/firebase_test.dart';
 
 void main() async {
   // Ensure Flutter bindings are initialized
-  WidgetsFlutterBinding.ensureInitialized();
 
-  // Initialize Firebase
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+
+  
 
   // Test Firebase connection (remove this in production)
-  await testFirebaseConnection();
+
 
   runApp(createApp());
 }
